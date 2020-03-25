1a34f15715bba3f5c88b0847f6c80a94fb5f00b2
Mamednoor <mamed.gohabur@gmail.com>
2020-03-25 12:27:43 +0100

    doing exercices

--- filter/ex1.js
+++ filter/ex1.js
@@ -14,7 +14,10 @@ Expected OUTPUT for this sample:
 */
 
 function getPositiveNumbers(numbers) {
+  return numbers.filter (newArr => newArr >= 0);
 }
+
+
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
